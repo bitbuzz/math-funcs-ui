8d1b43a8cdd4abb77d6a1353b3e9426c1b9a46f7
alex3867 <amuleh@esri.com>
2017-07-05 15:56:52 -0600

    Removed tabs and replaced with spaces.

--- MathFuncsUI/ScientificCalculatorControl.xaml.cs
+++ MathFuncsUI/ScientificCalculatorControl.xaml.cs
@@ -2,24 +2,24 @@
 
 namespace MathFuncsUI
 {
-	/// <summary>
-	/// Interaction logic for ScientificCalculatorControl.xaml
-	/// </summary>
-	public partial class ScientificCalculatorControl : UserControl
-	{
-		public ScientificCalculatorControl()
-		{
-			InitializeComponent();
-		}
+  /// <summary>
+  /// Interaction logic for ScientificCalculatorControl.xaml
+  /// </summary>
+  public partial class ScientificCalculatorControl : UserControl
+  {
+    public ScientificCalculatorControl()
+    {
+      InitializeComponent();
+    }
 
-		private void TextBoxCalculatorField_TextChanged(object sender, TextChangedEventArgs e)
-		{
-			if (((ViewModel)DataContext).IsScrollCalculatorFieldEnabled)
-			{
-				TextBoxCalculatorField.SelectionStart = TextBoxCalculatorField.Text.Length;
-				TextBoxCalculatorField.CaretIndex = TextBoxCalculatorField.Text.Length;
-				TextBoxCalculatorField.ScrollToEnd();
-			}
-		}
-	}
+    private void TextBoxCalculatorField_TextChanged(object sender, TextChangedEventArgs e)
+    {
+      if (((ViewModel)DataContext).IsScrollCalculatorFieldEnabled)
+      {
+        TextBoxCalculatorField.SelectionStart = TextBoxCalculatorField.Text.Length;
+        TextBoxCalculatorField.CaretIndex = TextBoxCalculatorField.Text.Length;
+        TextBoxCalculatorField.ScrollToEnd();
+      }
+    }
+  }
 }
