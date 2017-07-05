e3516604fa41f4a095c92f6d5e80d454e71a9e5e
bitbuzz <alexmuleh@gmail.com>
2017-07-05 16:44:28 -0600

    Bitbuzz/iss11 # (#12)
    * Add the important comment to ViewModel.cs
    
    * Added more comments

--- MathFuncsUI/ViewModel.cs
+++ MathFuncsUI/ViewModel.cs
@@ -47,6 +47,8 @@ namespace MathFuncsUI
 
     public ViewModel()
     {
+      // Really important comments for issue 11.
+      // More comments.
       _calculator = _calculatorFactory.CreateCalculator();
       _scientificCalculator = ((IScientificCalculator)_calculator);
     }
